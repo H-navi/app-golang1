adb8fbbd788dbc138fd3507a4e7a0ccbbf17f30d
H-navi <hanafinaufal@protonmail.com>
2023-10-27 22:34:31 +0700

    tag version module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/H-navi/app-golang1
+
+go 1.21.3
+
+require github.com/H-navi/golang1/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	go_hello_world "github.com/H-navi/golang1/v2"
+)
+
+func main() {
+	fmt.Println(go_hello_world.HelloWorld("Tono"))
+}
